8fd98cb928ee7248f5abeb567459421ef4c4556c
yosyo <mmoussou@student.42angouleme.fr>
2023-12-14 21:07:14 +0100

    Firefox Support, Coalition page fix, added a logo too :D

---
+++ background.js
@@ -0,0 +1,54 @@
+let seekersPatern = "https://cdn.intra.42.fr/coalition/cover/219/login-screen.jpg";
+const seekerstargetUrl = "https://cdn.discordapp.com/attachments/947139790183211018/1184336924031340594/seekers-login.png";
+
+
+let mmoussouPattern = "https://cdn.intra.42.fr/users/*/*mmoussou*";
+const mmoussoutargetUrl = "https://i.redd.it/a2nga4jvjy291.png";
+
+let lbouetPattern = "https://cdn.intra.42.fr/users/*/*lbouet*";
+const lbouetTargetUrl = "https://media1.tenor.com/m/n49_tZAbdocAAAAC/lain-iwakura-sel.gif";
+
+function seekersRedirect(requestDetails) {
+  if (requestDetails.url === seekerstargetUrl) {
+    return;
+  }
+  return {
+    redirectUrl: seekerstargetUrl,
+  };
+}
+
+function mmoussouRedirect(requestDetails) {
+  if (requestDetails.url === mmoussoutargetUrl) {
+    return;
+  }
+  return {
+    redirectUrl: mmoussoutargetUrl,
+  };
+}
+
+function lbouetRedirect(requestDetails) {
+  if (requestDetails.url === lbouetTargetUrl) {
+    return;
+  }
+  return {
+    redirectUrl: lbouetTargetUrl,
+  };
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  seekersRedirect,
+  { urls: [seekersPatern], types: ["image"] },
+  ["blocking"],
+);
+
+browser.webRequest.onBeforeRequest.addListener(
+  mmoussouRedirect,
+  { urls: [mmoussouPattern], types: ["image"] },
+  ["blocking"],
+);
+
+browser.webRequest.onBeforeRequest.addListener(
+  lbouetRedirect,
+  { urls: [lbouetPattern], types: ["image"] },
+  ["blocking"],
+);
---
+++ extra-styling.js
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.js                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yosyo <mmoussou@student.42angouleme.fr >   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 02:05:21 by yosyo             #+#    #+#             */
+/*   Updated: 2023/12/13 02:05:21 by yosyo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+var all = document.getElementsByTagName("*");
+
+function seekersUpdate()
+{
+    //var coalName = document.getElementsByClassName('coalition-span');
+    var all = document.getElementsByTagName("*");
+
+    for (var i=0, max=all.length; i < max; i++) {
+        if (all[i].style)
+        {
+            if (all[i].style.color == 'rgb(255, 105, 80)')
+            {
+                all[i].style.color = '#D950FF';
+            }
+            if (all[i].style.backgroundColor == 'rgb(255, 105, 80)')
+            {
+                all[i].style.backgroundColor = '#D950FF';
+            }
+            if (all[i].style.fill == 'rgb(255, 105, 80)')
+            {
+                all[i].style.fill = '#D950FF';
+            }
+        }
+    }
+}
+
+seekersUpdate();
+
+seekersUpdate();
