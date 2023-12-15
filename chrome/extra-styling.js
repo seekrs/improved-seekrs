87dca95a79c4fd918f339b609fcc5290dd838aef
Adam Joly <adjoly@2E2.42angouleme.fr>
2023-12-15 17:46:09 +0100

    added 0.4 functionality to chrome version

--- chrome/extra-styling.js
+++ chrome/extra-styling.js
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.js                                            :+:      :+:    :+:   */
+/*   extra-styling.js                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yosyo <mmoussou@student.42angouleme.fr >   +#+  +:+       +#+        */
+/*   By: adjoly <adjoly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 02:05:21 by yosyo             #+#    #+#             */
-/*   Updated: 2023/12/13 02:05:21 by yosyo            ###   ########.fr       */
+/*   Updated: 2023/12/15 17:45:28 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,3 +35,28 @@ function seekersUpdate()
 }
 
 seekersUpdate();
+
+function addTag(user, tag, color)
+{
+	pageUser = document.getElementsByClassName("login");
+	if (pageUser)
+		if (pageUser[0].textContent == user)
+			document.getElementsByClassName("name")[0].insertAdjacentHTML("beforebegin", "<span class='user-badge' style='background-color: " + color + "; border-color:" + color + "'>" + tag + "</span>\n &nbsp \n");
+}
+
+addTag("kiroussa", "kroussar", "#000000");
+
+async function applyBadges() {
+	const requestURL = "https://raw.githubusercontent.com/seekrs/improved-seekrs/main/badges.json";
+  	const request = new Request(requestURL);
+
+	const response = await fetch(request);
+	const badges = await response.json();
+
+	for (badge in badges)
+	{
+		addTag(badges[badge].user, badges[badge].tag, badges[badge].color);
+	}
+}
+
+applyBadges();
