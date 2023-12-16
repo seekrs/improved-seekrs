f08eb5e25151870f579c11ad0b4b4eae47b45c7b
killian <26600206+xtrm-en@users.noreply.github.com>
2023-12-16 01:30:50 +0100

    Imagine ça marche du 1er coup

--- background.js
+++ background.js
@@ -1,74 +1,59 @@
-let seekersPattern = "https://cdn.intra.42.fr/coalition/cover/219/login-screen.jpg";
-const seekersTargetUrl = "https://raw.githubusercontent.com/seekrs/art/main/wallpapers/seekrs-login.png";
-
-
-let mmoussouPattern = "https://cdn.intra.42.fr/users/*/*mmoussou*";
-const mmoussouTargetUrl = "https://media1.tenor.com/m/LcRQGhKX1AQAAAAd/ma-r%C3%A9action-sinc%C3%A8re-my-honest-reaction.gif";
-
-let lbouetPattern = "https://cdn.intra.42.fr/users/*/*lbouet*";
-const lbouetTargetUrl = "https://media1.tenor.com/m/n49_tZAbdocAAAAC/lain-iwakura-sel.gif";
-
-let kiroussaPattern = "https://cdn.intra.42.fr/users/*/*kiroussa*";
-const kiroussaTargetUrl = "https://media1.tenor.com/m/1G3k1aX57IUAAAAd/brain-damage.gif";
-
-function seekersRedirect(requestDetails) {
-  if (requestDetails.url === seekersTargetUrl) {
-    return;
+class OverrideTarget {
+  constructor(targetData) {
+    this.type = targetData.type;
+    this.value = targetData.value;
   }
-  return {
-    redirectUrl: seekersTargetUrl,
-  };
+
+  get url() {
+    return this.url;
+  }
+
+  get types() {
+    if (this.type === "userProfile") {
+      return ["image"];
+    }
+    if (this.type === "pattern") {
+      if (this.url.endsWith(".jpg") || this.url.endsWith(".png") || this.url.endsWith(".jpeg") || this.url.endsWith(".gif") || this.url.endsWith(".webp")) {
+        return ["image"];
+      }
+    }
+    return [];
+  }
+
+  get filter() {
+    return { urls: [this.url], types: this.types };
+  }
 }
 
-function mmoussouRedirect(requestDetails) {
-  if (requestDetails.url === mmoussouTargetUrl) {
-    return;
+class Override {
+  constructor(target, replaceBy) {
+    this.target = new OverrideTarget(target);
+    this.replaceBy = replaceBy;
   }
-  return {
-    redirectUrl: mmoussouTargetUrl,
-  };
+
+  apply() {
+    browser.webRequest.onBeforeRequest.addListener(
+      (req) => {
+        if (req.url === this.replaceBy) return {};
+        return { redirectUrl: this.replaceBy };
+      },
+      this.target.filter,
+      ["blocking"],
+    );
+    console.log("✅ Applied redirection for " + this.target.url + " -> " + this.replaceBy);
+  }
 }
 
-function lbouetRedirect(requestDetails) {
-  if (requestDetails.url === lbouetTargetUrl) {
-    return;
-  }
-  return {
-    redirectUrl: lbouetTargetUrl,
-  };
+async function applyWebFilters() {
+	const requestURL = "https://raw.githubusercontent.com/seekrs/improved-seekrs/feature/json-backgrounds/data/requests-overrides.json";
+  const request = new Request(requestURL);
+
+	const response = await fetch(request);
+	const overrides = await response.json();
+
+	for (override in overrides) {
+		Override(override).apply();
+	}
 }
 
-function kiroussaRedirect(requestDetails) {
-  if (requestDetails.url === kiroussaTargetUrl) {
-    return;
-  }
-  return {
-    redirectUrl: kiroussaTargetUrl,
-  };
-}
-
-// ---------------------------------------------------
-
-browser.webRequest.onBeforeRequest.addListener(
-  seekersRedirect,
-  { urls: [seekersPattern], types: ["image"] },
-  ["blocking"],
-);
-
-browser.webRequest.onBeforeRequest.addListener(
-  mmoussouRedirect,
-  { urls: [mmoussouPattern], types: ["image"] },
-  ["blocking"],
-);
-
-browser.webRequest.onBeforeRequest.addListener(
-  lbouetRedirect,
-  { urls: [lbouetPattern], types: ["image"] },
-  ["blocking"],
-);
-
-browser.webRequest.onBeforeRequest.addListener(
-  kiroussaRedirect,
-  { urls: [kiroussaPattern], types: ["image"] },
-  ["blocking"],
-);
+applyWebFilters();
