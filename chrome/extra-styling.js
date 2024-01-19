9e35b54c3d3db84474010ef20e27c2878f8abce0
Adam Joly <adjoly@2E2.42angouleme.fr>
2024-01-19 11:54:12 +0100

    add a little bit of racism

--- chrome/extra-styling.js
+++ chrome/extra-styling.js
@@ -1,22 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.js                                            :+:      :+:    :+:   */
+/*   extra-styling.js                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yosyo <mmoussou@student.42angouleme.fr >   +#+  +:+       +#+        */
+/*   By: adjoly <adjoly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 02:05:21 by yosyo             #+#    #+#             */
-/*   Updated: 2023/12/13 02:05:21 by yosyo            ###   ########.fr       */
+/*   Updated: 2024/01/18 15:58:32 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 var all = document.getElementsByTagName("*");
-
-var css = ":root { \n	--theme-color: #D950FF;\n	--theme-color-dark: #9d36ad;\n --theme-color-light: #e99cff;\n --logtime-chart-24h-color: #e99cff;\n}";
-
-var styleSheet = document.createElement("style");
-styleSheet.innerText = css;
-document.head.appendChild(styleSheet);
 
 function seekersUpdate()
 {
@@ -45,48 +39,46 @@ seekersUpdate();
 function addTag(user, tag, color)
 {
 	pageUser = document.getElementsByClassName("login");
-	if (pageUser.length > 0) {
-		if (pageUser[0].dataset.login === user) {
+	if (pageUser)
+		if (pageUser[0].textContent == user)
 			document.getElementsByClassName("name")[0].insertAdjacentHTML("beforebegin", "<span class='user-badge' style='background-color: " + color + "; border-color:" + color + "'>" + tag + "</span>\n &nbsp \n");
-		}
-	}
 }
+
 
 async function applyBadges() {
 	const requestURL = "https://raw.githubusercontent.com/seekrs/improved-seekrs/main/data/badges.json";
-  	const request = new Request(requestURL);
-
+	const request = new Request(requestURL);
+	
 	const response = await fetch(request);
 	const badges = await response.json();
-
-	for (id in badges) {
-		badge = badges[id];
-		addTag(badge.user, badge.tag, badge.color);
+	
+	for (badge in badges)
+	{
+		addTag(badges[badge].user, badges[badge].tag, badges[badge].color);
 	}
 }
+addTag("kiroussa", "kroussar", "#000000");
 
 applyBadges();
 
 function addTitle(user, prefix, suffix, format)
 {
-	if (format === undefined) format = "%prefix% %user% %suffix%";
-	if (prefix === undefined) prefix = "";
-	if (suffix === undefined) suffix = "";
-	
-	let titledName = format.replace("%prefix%", prefix).replace("%user%", user).replace("%suffix%", suffix);
 	pageUser = document.getElementsByClassName("login");
-	if (pageUser.length > 0) {
-		if (pageUser[0].dataset.login === user) {
-			pageUser[0].textContent = titledName;
+	if (pageUser) {
+		if (pageUser[0].textContent == user) {
+			if (format === undefined) {
+				format = "%prefix% %user% %suffix%";
+			}
+			if (prefix === undefined) {
+				prefix = "";
+			}
+			if (suffix === undefined) {
+				suffix = "";
+			}
+			pageUser[0].textContent = format.replace("%prefix%", prefix).replace("%user%", user).replace("%suffix%", suffix);
 		}	
 	}
-	document.querySelectorAll("#collapseEvaluations .reminder .project-item-text a").forEach((aTag) => {
-		if (aTag.dataset.userLink === user) {
-			aTag.textContent = titledName;
-		}
-	});
 }
-
 
 async function applyTitles() {
 	const requestURL = "https://raw.githubusercontent.com/seekrs/improved-seekrs/main/data/titles.json";
